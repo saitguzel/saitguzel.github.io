e25b1effd8d278c4d7f3555f25cef07fd1542439
Sait GÜZEL <saitguzel90@hotmail.com>
2023-10-26 16:10:06 +0300

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,67 @@
+(function ($) {
+	
+	"use strict";
+
+	$(".Modern-Slider").slick({
+	    autoplay:true,
+	    autoplaySpeed:10000,
+	    speed:600,
+	    slidesToShow:1,
+	    slidesToScroll:1,
+	    pauseOnHover:false,
+	    dots:true,
+	    pauseOnDotsHover:true,
+	    cssEase:'linear',
+	   // fade:true,
+	    draggable:false,
+	    prevArrow:'<button class="PrevArrow"></button>',
+	    nextArrow:'<button class="NextArrow"></button>', 
+	  });
+
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+	
+
+	// Page loading animation
+	$(window).on('load', function() {
+		if($('.cover').length){
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function(){
+			setTimeout(function(){
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function() {
+		mobileNav();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		var width = $(window).width();
+		$('.submenu').on('click', function() {
+			if(width < 767) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+})(window.jQuery);
